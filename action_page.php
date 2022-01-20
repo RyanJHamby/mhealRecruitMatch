19979ca55dd1c09739a564cb29cc0cc7d3de20cb
Ryan Hamby <hambyr@umich.edu>
2022-01-20 12:40:13 -0500

    c

--- action_page.php
+++ action_page.php
@@ -1,8 +1,10 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome 
+<!-- <?php echo $_POST["name"]; ?><br> -->
+Your email address is: 
+<!-- <?php echo $_POST["email"]; ?> -->
 
 </body>
 </html>

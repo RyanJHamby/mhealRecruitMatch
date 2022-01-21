d2a58598ac99d98cae4c1faf1d7fb6a75ff4589c
Ryan Hamby <hambyr@umich.edu>
2022-01-20 22:27:44 -0500

    a

--- script.js
+++ script.js
@@ -44,57 +44,59 @@ function UploadProcessTeam() {
 };
 
 function GetTableFromTeamExcel(data) {
+    xlsxToCSV(data);
     //Read the Excel File data in binary 
     var workbook = XLSX.read(data, { type: 'binary' });
 
-    //get the name of First Sheet. 
-    var Sheet = workbook.SheetNames[0];
+    for (var i = 0; i < workbook.SheetNames.length; ++i) {
+        //get the name of First Sheet. 
+        var Sheet = workbook.SheetNames[i];
 
-    //Read all rows from First Sheet into an JSON array.
-    var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[Sheet]);
-    var ExcelTable = document.getElementById("ExcelTable");
+        //Read all rows from First Sheet into an JSON array.
+        var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[Sheet]);
+        var ExcelTable = document.getElementById("ExcelTable");
 
-    //Create a HTML Table element. 
-    var myTable = document.createElement("table");
-    myTable.border = "1";
+        //Create a HTML Table element. 
+        var myTable = document.createElement("table");
+        myTable.border = "1";
 
-    //Add the header row. 
-    var row = myTable.insertRow(-1);
-
-    //Add the header cells. 
-    var headerCell = document.createElement("TH");
-    headerCell.innerHTML = "Email";
-    row.appendChild(headerCell);
-
-    headerCell = document.createElement("TH");
-    headerCell.innerHTML = "Rank";
-    row.appendChild(headerCell);
-
-    headerCell = document.createElement("TH");
-    headerCell.innerHTML = "FirstName";
-    row.appendChild(headerCell);
-
-    headerCell = document.createElement("TH");
-    headerCell.innerHTML = "LastName";
-    row.appendChild(headerCell);
-
-    //Add the data rows from Excel file. 
-    for (var i = 0; i < excelRows.length; i++) {
-        //Add the data row. 
+        //Add the header row. 
         var row = myTable.insertRow(-1);
-        //Add the data cells. 
-        cell = row.insertCell(-1);
-        cell.innerHTML = excelRows[i].Email;
-        cell = row.insertCell(-1);
-        cell.innerHTML = excelRows[i].Rank;
-        cell = row.insertCell(-1);
-        cell.innerHTML = excelRows[i].FirstName;
-        cell = row.insertCell(-1);
-        cell.innerHTML = excelRows[i].LastName;
+
+        //Add the header cells. 
+        var headerCell = document.createElement("TH");
+        headerCell.innerHTML = "Email";
+        row.appendChild(headerCell);
+
+        headerCell = document.createElement("TH");
+        headerCell.innerHTML = "Rank";
+        row.appendChild(headerCell);
+
+        headerCell = document.createElement("TH");
+        headerCell.innerHTML = "FirstName";
+        row.appendChild(headerCell);
+
+        headerCell = document.createElement("TH");
+        headerCell.innerHTML = "LastName";
+        row.appendChild(headerCell);
+
+        //Add the data rows from Excel file. 
+        for (var j = 0; j < excelRows.length; j++) {
+            //Add the data row. 
+            var row = myTable.insertRow(-1);
+            //Add the data cells. 
+            cell = row.insertCell(-1);
+            cell.innerHTML = excelRows[j].Email;
+            cell = row.insertCell(-1);
+            cell.innerHTML = excelRows[j].Rank;
+            cell = row.insertCell(-1);
+            cell.innerHTML = excelRows[j].FirstName;
+            cell = row.insertCell(-1);
+            cell.innerHTML = excelRows[j].LastName;
+        }
+        var ExcelTable = document.getElementById("ExcelTable");
+        ExcelTable.append("Team: " + workbook.SheetNames[i], myTable);
     }
-    var ExcelTable = document.getElementById("ExcelTable");
-    document.getElementById("ExcelTable").innerHTML = "";
-    ExcelTable.appendChild(myTable);
 };
 
 function UploadProcessApplicant() {
@@ -133,15 +135,16 @@ function GetTableFromApplicantExcel(data) {
     var workbook = XLSX.read(data, { type: 'binary' });
 
     //get the name of First Sheet. 
-    for (int i = 0; i < SheetNames.size(); ++i) {
+    for (var i = 0; i < workbook.SheetNames.length; ++i) {
         var Sheet = workbook.SheetNames[i];
 
         //Read all rows from First Sheet into an JSON array.
         var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[Sheet]);
-        var ExcelTable = document.getElementById("ExcelTable");
+        var ExcelTable = document.getElementById("ExcelTable2");
 
         //Create a HTML Table element. 
         var myTable = document.createElement("table");
+        myTable.border = 1;
 
         //Add the header row. 
         var row = myTable.insertRow(-1);
@@ -173,18 +176,58 @@ function GetTableFromApplicantExcel(data) {
             var row = myTable.insertRow(-1);
             //Add the data cells. 
             cell = row.insertCell(-1);
-            cell.innerHTML = excelRows[i].Email;
+            cell.innerHTML = excelRows[j].Email;
             cell = row.insertCell(-1);
-            cell.innerHTML = excelRows[i].Ranking;
+            cell.innerHTML = excelRows[j].Ranking;
             cell = row.insertCell(-1);
-            cell.innerHTML = excelRows[i].Team;
+            cell.innerHTML = excelRows[j].Team;
             cell = row.insertCell(-1);
-            cell.innerHTML = excelRows[i].FirstName;
+            cell.innerHTML = excelRows[j].FirstName;
             cell = row.insertCell(-1);
-            cell.innerHTML = excelRows[i].LastName;
+            cell.innerHTML = excelRows[j].LastName;
         }
         var ExcelTable = document.getElementById("ExcelTable2");
-        document.getElementById("ExcelTable2").innerHTML = "";
-        ExcelTable.appendChild(myTable);
+        ExcelTable.append("Applicant: " + workbook.SheetNames[i], myTable);
     }
 };
+
+
+function xlsxToCSV(data) {
+    var writeStr = "a"
+    var rows = []
+    //var fs = require("fs");
+
+    //Read the Excel File data in binary 
+    var workbook = XLSX.read(data, { type: 'binary' });
+    // //get the name of First Sheet. 
+    // for (var i = 0; i < workbook.SheetNames.length; ++i) {
+    //     var Sheet = workbook.SheetNames[i];
+
+    //     //Read all rows from First Sheet into an JSON array.
+    //     var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[Sheet]);
+
+    //     for (var r = 0; r < excelRows.length; ++r) {
+    //         rows.push(Sheet['data'][r]);
+    //     }
+    //     for (var j = 0; j < excelRows.length; j++) {
+    //         writeStr += excelRows[r].join(",") + "\n";
+    //     }
+    // }
+    //var fileCSV = XLSX.utils.book_to_csv(workbook)
+    let worksheet = workbook.SheetNames[workbook.SheetNames[0]]
+    //var new_workbook = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(workbook, worksheet, "CSV_Sheet")
+    XLSX.writeFile(workbook, "/test.csv")
+    document.getElementById("a").innerHTML = writeStr
+
+
+    //writes to a file, but you will presumably send the csv as a      
+    //response instead
+    // fs.writeFile(__dirname + "/test.csv", writeStr, function (err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.log("test.csv was saved in the current directory!");
+    // }); 
+
+}

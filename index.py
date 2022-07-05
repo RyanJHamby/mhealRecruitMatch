77c49c40b400c5a670bb6badc0da1a2593708418
Ryan Hamby <hambyr@umich.edu>
2022-07-04 21:33:20 -0400

    a

--- index.py
+++ index.py
@@ -9,12 +9,12 @@ app.testing = True
 @app.route('/', methods=['POST', 'GET'])
 def index():
 	if request.method == 'GET':
-	return render_template('index.html', result={})
+		return render_template('index.html', result={})
 	if request.method == 'POST':
-	form = request.form
-	file_ptr = mhealMatch()
-	print(file_ptr)
-	return render_template('index.html', result=file_ptr)
+		form = request.form
+		file_ptr = mhealMatch()
+		print(file_ptr)
+		return render_template('index.html', result=file_ptr)
 	return render_template('index.html', result={})
 
 
@@ -22,13 +22,13 @@ def mhealMatch():
 		# render in results
 	results = {}
 	if request.files['TeamRankings'].filename == '':
-	results['error'] = "Please upload a valid Teams_Ranking_Applicants .csv file"
-	return results
+		results['error'] = "Please upload a valid Teams_Ranking_Applicants .csv file"
+		return results
 	if request.files['ApplicantRankings'].filename == '':
-	results['error'] = "Please upload a valid Applicants_Ranking_Teams .csv file"
-	return results
+		results['error'] = "Please upload a valid Applicants_Ranking_Teams .csv file"
+		return results
 	if request.files['Spots'].filename == '':
-	results['error'] = "Please upload a valid Team_Places .csv file"
+		results['error'] = "Please upload a valid Team_Places .csv file"
 	return results
 	team = request.files['TeamRankings'].read()
 	applicant = request.files['ApplicantRankings'].read()
@@ -36,12 +36,12 @@ def mhealMatch():
 	match = MatchController(team, applicant, spots)
 	setup_error = match.organize_candidates(spots)
 	if setup_error != "":
-	results["error"] = setup_error
-	return results
+		results["error"] = setup_error
+		return results
 	match_error = match.start_match()
 	if match_error != "":
-	results["error"] = match_error
-	return results
+		results["error"] = match_error
+		return results
 	results = match.results_dict()
 	return results
 

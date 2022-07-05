aa2d7e3865a35ec1c73684339ff5db361cd00e18
Ryan Hamby <hambyr@umich.edu>
2022-07-04 21:22:34 -0400

    b

--- RothPeransonMaster/match/RothPeranson.py
+++ RothPeransonMaster/match/RothPeranson.py
@@ -2,14 +2,15 @@ import pandas as pd
 import csv
 import sys
 import io
-if sys.version_info[0] < 3: 
+if sys.version_info[0] < 3:
     from StringIO import StringIO
 else:
     from io import StringIO
 import traceback
 
+
 class Student():
-# Class representing someone making an application.
+    # Class representing someone making an application.
 
     # Commented out sections describe the process loudly
     def __init__(self, name, choices):
@@ -39,8 +40,9 @@ class Student():
 
         self.find_next()
 
+
 class Program():
-# Class representing some program accepting total_places students
+    # Class representing some program accepting total_places students
 
     def __init__(self, name, total_places=1):
         self.name = name
@@ -60,7 +62,8 @@ class Program():
         if candidate in self.choices:
             if len(self.current_picks) < self.total_places:
                 self.current_picks.append(candidate)
-                self.current_picks = sorted(self.current_picks, key=lambda r: self.choices.index(r))
+                self.current_picks = sorted(
+                    self.current_picks, key=lambda r: self.choices.index(r))
                 candidate.current_place = self
                 return True
 
@@ -79,52 +82,52 @@ class Program():
 
 
 class MatchController():
-# This class manages the processing of rank order lists for Students and Programs
-# in addition to controlling the match process and returning the final results.
+    # This class manages the processing of rank order lists for Students and Programs
+    # in addition to controlling the match process and returning the final results.
 
     def __init__(self, program_data, candidate_data, places_data=None):
+        # Takes csv data directories
+        program_data_str = StringIO(program_data.decode(
+            'utf-8').lower().replace(' ', '').replace('\n', '').replace('\t', ''))
+        candidate_data_str = StringIO(candidate_data.decode(
+            'utf-8').lower().replace(' ', '').replace('\n', '').replace('\t', ''))
+        # print(program_data_str)
+        # self.program_data = csv.reader(program_data_str, delimiter=',')
+        # for row in reader:
+        #     print(row)
+        # with open(program_data_str, newline='') as csvfile:
+        #     a = csv.DictReader(open(program_data))
+        #     for row in a:
+        #         print(row)
+        # self.candidate_data = csv.reader(candidate_data_str, delimiter=',')
+        self.program_data = pd.read_csv(program_data_str)
+        self.candidate_data = pd.read_csv(candidate_data_str)
+        self.candidate_data = self.candidate_data.drop(
+            self.candidate_data.index[0])
+        self.candidate_data = self.candidate_data.drop(
+            self.candidate_data.index[0])
+        if places_data:
+            places_data_str = StringIO(places_data.decode('utf-8'))
+            # self.places_data = csv.reader(places_data_str, delimiter=',')
+            self.places_data = pd.read_csv(
+                places_data_str, names=[1, 2], index_col=0)
+
+        self.programs = {}
+        self.candidates = {}
+        # print(type(self.candidate_data))
+        # print(self.program_data)
+        # print(self.candidate_data)
+        # print(self.places_data)
+
+    def organize_candidates(self, places_data):
         try:
-            # Takes csv data directories
-            program_data_str = StringIO(program_data.decode('utf-8'))
-            program_data_str = program_data_str.lower()
-            candidate_data_str = StringIO(candidate_data.decode('utf-8'))
-            candidate_data_str = candidate_data_str.lower()
-            for item in program_data_string.split(','):
-                item.strip()
-            for item in candidate_data_string.split(','):
-                item.strip()
-            # print(program_data.splitlines())
-            # print(program_data_str)
-            # self.program_data = csv.reader(program_data_str, delimiter=',')
-
-            # for row in reader:
-            #     print(row)
-            # with open(program_data_str, newline='') as csvfile:
-            #     a = csv.DictReader(open(program_data))
-            #     for row in a:
-            #         print(row)
-            self.candidate_data = csv.reader(candidate_data_str, delimiter=',')
-            self.program_data = pd.read_csv(program_data_str)
-            self.candidate_data = pd.read_csv(candidate_data_str)
-            self.candidate_data = self.candidate_data.drop(self.candidate_data.index[0])
-            self.candidate_data = self.candidate_data.drop(self.candidate_data.index[0])
-            if places_data:
-                places_data_str = StringIO(places_data.decode('utf-8'))
-                # self.places_data = csv.reader(places_data_str, delimiter=',')
-                self.places_data = pd.read_csv(places_data_str,names=[1,2],index_col=0)
-
-            self.programs = {}
-            self.candidates = {}
-            print(type(self.candidate_data))
-            print(self.program_data)
-            print(self.candidate_data)
-            print(self.places_data)
             for c in self.program_data.columns:
-                print(c)
-                print(self.program_data.columns)
-                print(self.places_data.loc[c].at[2])
+                # print(c)
+                # print(self.program_data.columns)
+                # print(self.places_data.loc[c].at[2])
                 if places_data:
-                    self.programs[c] = Program(c, self.places_data.loc[c].at[2])
+                    self.programs[c] = Program(
+                        c, self.places_data.loc[c].at[2])
                 else:
                     self.programs[c] = Program(c)
 
@@ -135,7 +138,8 @@ class MatchController():
 
             for c in self.program_data.columns:
                 choices = self.program_data[c].dropna().tolist()
-                choice_objects = [self.candidates[k] for k in choices] #changed c to k
+                choice_objects = [self.candidates[k]
+                                  for k in choices]  # changed c to k
                 self.programs[c].choices = choice_objects
 
             for c in self.candidate_data:
@@ -143,8 +147,8 @@ class MatchController():
             for c in self.places_data:
                 print(c)
         except:
-            setup_error_message = traceback.format_exc()
-            return setup_error_message
+            results_error_message = traceback.format_exc()
+            return results_error_message
 
     def start_match(self):
         try:
@@ -194,4 +198,3 @@ class MatchController():
         results_df.columns = ['Candidate', 'Matched Program']
 
         results_df.to_csv('/results.csv', index=False)
-
